1ba2ba657d5c3ff0664fbae4dd340b7bcf583d5a
saviodot <59295670+saviodot@users.noreply.github.com>
2021-01-28 07:22:27 +0800

    Update ungit.sh

--- resources/tools/ungit.sh
+++ resources/tools/ungit.sh
@@ -17,6 +17,7 @@ done
 if ! hash ungit 2>/dev/null; then
     echo "Installing Ungit. Please wait..."
     npm update
+    npm install -g mkdirp@1.0.4
     npm install -g ungit@1.5.9
 else
     echo "Ungit is already installed"
